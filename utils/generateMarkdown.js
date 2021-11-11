c28c3c4cda9adde0b455bcea5278cc517fac2abe
Tina Ly <t.tinaly@hotmail.com>
2021-11-10 23:49:57 -0800

    license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function makeLicense (selection){
+  for (let i = 0; i < licenseObjs.length; i++){
+  if (selection === licenseObjs[i].name) return licenseObjs[i];
+}
+return "No License";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
