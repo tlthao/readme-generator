234e8d1817a57c2fc89069b447031b4d5fb2cf22
Tina Ly <t.tinaly@hotmail.com>
2021-11-10 23:05:59 -0800

    adding questions

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 // TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const license = require ('./utils/generateMarkdown')
-// TODO: Create an array of questions for user input
-const questions = [];
 
+// TODO: Create an array of questions for user input
+
+const questions = () => {
+    return inquirer.prompt ([
+      {
+          type: 'input',
+          name: 'title',
+          message: 'What is the project title?',
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Please provide a description of the project.',
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'Please provide installation instructions.',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'Please provide usage information',
+        },
+        {
+          type: 'input',
+          name: 'contribution',
+          message: 'Please provide contribution guidelines',
+        },
+        {
+          type: 'input',
+          name: 'test',
+          message: 'Please provide test instructions',
+        },
+        {
+          type: 'list',
+          name: 'license',
+          message: 'Please select license',
+          choices: ['No License', 'Apache', 'Boost', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'IBM', 'ISC', 'MIT', 'Mozilla']
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Please enter github username.',
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'Please enter email address.',
+        },
+    ])
+  };
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
