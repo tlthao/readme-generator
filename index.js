aa7d99d93827764fdbab3e139318a4d865c7030f
Tina Ly <t.tinaly@hotmail.com>
2021-11-10 22:52:21 -0800

    adding packages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const license = require ('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
 
